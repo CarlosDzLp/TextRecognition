66e37697ba3f2258e9957a425883c16103b8977a
Carlos Diaz <ryankar90@hotmail.com>
2020-12-13 22:02:03 -0600

    create project

---
+++ CameraRecognitionText/CameraRecognitionText/MainActivity.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Text;
+using Android;
+using Android.App;
+using Android.Gms.Vision;
+using Android.Gms.Vision.Texts;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using AndroidX.Core.App;
+
+namespace CameraRecognitionText
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, ISurfaceHolderCallback, Detector.IProcessor,Java.Lang.IRunnable
+    {
+        SurfaceView mCameraView;
+        TextView mTextView;
+        TextView mTextViewLanguage;
+        CameraSource mCameraSource;
+        Button btn;
+        private static int requestPermissionID = 101;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            mCameraView = FindViewById<SurfaceView>(Resource.Id.surfaceView);
+            mTextView = FindViewById<TextView>(Resource.Id.text_view);
+            mTextViewLanguage = FindViewById<TextView>(Resource.Id.text_viewlanguaje);
+            btn = FindViewById<Button>(Resource.Id.btn);
+            btn.Click += Btn_Click;           
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (btn.Text == "Iniciar")
+                {
+                    btn.Text = "Detener";
+                    startCameraSource();
+                }
+                else
+                {
+                    btn.Text = "Iniciar";
+                    mCameraSource.Stop();
+                    textRecognizer = null;
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        public void SurfaceChanged(ISurfaceHolder holder, [GeneratedEnum] Format format, int width, int height)
+        {
+
+        }
+
+        public void SurfaceCreated(ISurfaceHolder holder)
+        {
+            try
+            {
+                if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.Camera) != Android.Content.PM.Permission.Granted)
+                {
+
+                    ActivityCompat.RequestPermissions(this,
+                            new String[] { Manifest.Permission.Camera },
+                            requestPermissionID);
+                    return;
+                }                
+                mCameraSource.Start(mCameraView.Holder);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            }
+        }
+
+        public void SurfaceDestroyed(ISurfaceHolder holder)
+        {
+
+        }
+
+        SparseArray items;
+        public void ReceiveDetections(Detector.Detections detections)
+        {
+            try
+            {
+                items = detections.DetectedItems;
+                if (items.Size() != 0)
+                {
+                    mTextView.Post(this);
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        public void Run()
+        {
+            try
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                string language = "";
+                for (int i = 0; i < items.Size(); i++)
+                {
+                    var item = items.ValueAt(i) as TextBlock;
+                    language = item.Language;
+                    stringBuilder.Append(item.Value);
+                    stringBuilder.Append("\n");
+                }
+                mTextView.Text = stringBuilder.ToString();
+                mTextViewLanguage.Text = language;
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        public void Release()
+        {
+            //NO SE  HACE  NADA
+        }
+
+        TextRecognizer textRecognizer;
+        private void startCameraSource()
+        {
+            try
+            {
+                textRecognizer = new TextRecognizer.Builder(this).Build();
+                if (!textRecognizer.IsOperational)
+                {
+                    System.Diagnostics.Debug.WriteLine("Detector dependencies not loaded yet");
+                }
+                else
+                {
+                    mCameraSource = new CameraSource.Builder(this, textRecognizer)
+                        .SetFacing(CameraFacing.Back)
+                        .SetRequestedPreviewSize(1280, 1024)
+                        .SetAutoFocusEnabled(true)
+                        .SetRequestedFps(2.0f)
+                        .Build();
+                    mCameraView.Holder.AddCallback(this);
+                    textRecognizer.SetProcessor(this);
+                    if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.Camera) == Android.Content.PM.Permission.Granted)
+                    {                      
+                        mCameraSource.Start(mCameraView.Holder);
+                    }
+                    else
+                    {
+                        ActivityCompat.RequestPermissions(this,
+                                new String[] { Manifest.Permission.Camera },
+                                requestPermissionID);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+            if(grantResults[0] == Android.Content.PM.Permission.Granted)
+            {
+                try
+                {
+                    if(ActivityCompat.CheckSelfPermission(this,Manifest.Permission.Camera)== Android.Content.PM.Permission.Granted)
+                    {
+                        mCameraSource.Start(mCameraView.Holder);
+                    }
+                }
+                catch(Exception ex)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ CameraRecognitionText/CameraRecognitionText/obj/Debug/android/src/crc6438f4534eeed9d29c/MainActivity.java
@@ -0,0 +1,121 @@
+package crc6438f4534eeed9d29c;
+
+
+public class MainActivity
+	extends androidx.appcompat.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer,
+		android.view.SurfaceHolder.Callback,
+		com.google.android.gms.vision.Detector.Processor,
+		java.lang.Runnable
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onRequestPermissionsResult:(I[Ljava/lang/String;[I)V:GetOnRequestPermissionsResult_IarrayLjava_lang_String_arrayIHandler\n" +
+			"n_surfaceChanged:(Landroid/view/SurfaceHolder;III)V:GetSurfaceChanged_Landroid_view_SurfaceHolder_IIIHandler:Android.Views.ISurfaceHolderCallbackInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_surfaceCreated:(Landroid/view/SurfaceHolder;)V:GetSurfaceCreated_Landroid_view_SurfaceHolder_Handler:Android.Views.ISurfaceHolderCallbackInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_surfaceDestroyed:(Landroid/view/SurfaceHolder;)V:GetSurfaceDestroyed_Landroid_view_SurfaceHolder_Handler:Android.Views.ISurfaceHolderCallbackInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_receiveDetections:(Lcom/google/android/gms/vision/Detector$Detections;)V:GetReceiveDetections_Lcom_google_android_gms_vision_Detector_Detections_Handler:Android.Gms.Vision.Detector/IProcessorInvoker, Xamarin.GooglePlayServices.Vision.Common\n" +
+			"n_release:()V:GetReleaseHandler:Android.Gms.Vision.Detector/IProcessorInvoker, Xamarin.GooglePlayServices.Vision.Common\n" +
+			"n_run:()V:GetRunHandler:Java.Lang.IRunnableInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"";
+		mono.android.Runtime.register ("CameraRecognitionText.MainActivity, CameraRecognitionText", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("CameraRecognitionText.MainActivity, CameraRecognitionText", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("CameraRecognitionText.MainActivity, CameraRecognitionText", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2)
+	{
+		n_onRequestPermissionsResult (p0, p1, p2);
+	}
+
+	private native void n_onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2);
+
+
+	public void surfaceChanged (android.view.SurfaceHolder p0, int p1, int p2, int p3)
+	{
+		n_surfaceChanged (p0, p1, p2, p3);
+	}
+
+	private native void n_surfaceChanged (android.view.SurfaceHolder p0, int p1, int p2, int p3);
+
+
+	public void surfaceCreated (android.view.SurfaceHolder p0)
+	{
+		n_surfaceCreated (p0);
+	}
+
+	private native void n_surfaceCreated (android.view.SurfaceHolder p0);
+
+
+	public void surfaceDestroyed (android.view.SurfaceHolder p0)
+	{
+		n_surfaceDestroyed (p0);
+	}
+
+	private native void n_surfaceDestroyed (android.view.SurfaceHolder p0);
+
+
+	public void receiveDetections (com.google.android.gms.vision.Detector.Detections p0)
+	{
+		n_receiveDetections (p0);
+	}
+
+	private native void n_receiveDetections (com.google.android.gms.vision.Detector.Detections p0);
+
+
+	public void release ()
+	{
+		n_release ();
+	}
+
+	private native void n_release ();
+
+
+	public void run ()
+	{
+		n_run ();
+	}
+
+	private native void n_run ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
